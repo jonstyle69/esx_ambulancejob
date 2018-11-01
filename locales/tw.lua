193b55382b836b177f5c28b3413be125d61d4f04
jonstyle69 <44425507+jonstyle69@users.noreply.github.com>
2018-11-01 17:26:27 +0800

    Traditional Chinese Translation

---
+++ locales/tw.lua
@@ -0,0 +1,63 @@
+Locales['tw'] = {
+  -- Cloakroom
+  ['cloakroom'] = '更衣室',
+  ['ems_clothes_civil'] = '一般服裝',
+  ['ems_clothes_ems'] = '救護員服裝',
+  -- Vehicles
+  ['veh_menu'] = '車輛',
+  ['veh_spawn'] = '按~INPUT_CONTEXT~獲取救護車或直升機',
+  ['store_veh'] = '按~INPUT_CONTEXT~來存儲車輛',
+  ['ambulance'] = '救護車',
+  ['helicopter'] = '直升機',
+  -- Action Menu
+  ['hospital'] = '醫院',
+  ['revive_inprogress'] = '正在復活',
+  ['revive_complete'] = '你已經復活了',
+  ['no_players'] = '附近沒有玩家',
+  ['no_vehicles'] = '附近沒有車輛',
+  ['isdead'] = '死亡',
+  ['unconscious'] = '無生命跡象',
+  -- Boss Menu
+  ['deposit_society'] = 'eMS 存取',
+  ['withdraw_society'] = 'eMS 取出',
+  ['boss_actions'] = '老闆行動',
+  -- Misc
+  ['invalid_amount'] = '~r~無效金額',
+  ['open_menu'] = '按~INPUT_CONTEXT~打開選單',
+  ['deposit_amount'] = '存款金額',
+  ['money_withdraw'] = '提款金額',
+  ['fast_travel'] = '按~INPUT_CONTEXT~快速旅行',
+  ['open_pharmacy'] = '按~INPUT_CONTEXT~打開藥局',
+  ['pharmacy_menu_title'] = '藥局',
+  ['pharmacy_take'] = '拿',
+  ['medikit'] = '醫療包',
+  ['bandage'] = '繃帶',
+  ['max_item'] = '你已經足夠了',
+  -- F6 Menu
+  ['ems_menu'] = '救護選單',
+  ['ems_menu_title'] = '救護員 - 選單',
+  ['ems_menu_revive'] = '復活玩家',
+  ['ems_menu_putincar'] = '放入車輛',
+  ['ems_menu_small'] = '治愈小傷口',
+  ['ems_menu_big'] = '治療嚴重傷口',
+  -- Phone
+  ['alert_ambulance'] = '救護車警報',
+  -- Death
+  ['respawn_at_hospital'] = '你想強制在醫院重生嗎?',
+  ['yes'] = '是',
+  ['please_wait'] = '你將會在失血過多中昏迷 ~b~',
+  ['minutes'] = ' 分鐘 ',
+  ['seconds_fine'] = ' 秒 ~w~強制重生，現在按 ~g~$',
+  ['seconds'] = ' 秒',
+  ['press_respawn_fine'] = '~w~ [按 ~b~E~w~]',
+  ['press_respawn'] = '按 [~b~E~w~] 來重生',
+  -- Revive
+  ['revive_help'] = '治療一位玩家',
+  -- Item
+  ['used_medikit'] = '你已使用 1x 醫療包',
+  ['used_bandage'] = '你已使用  1x 繃帶',
+  ['not_enough_medikit'] = '你沒有 ~b~醫療包~w~.',
+  ['not_enough_bandage'] = '你沒有 ~b~繃帶~w~.',
+  ['healed'] = '你獲得了治癒',
+  -- Misc
+}
