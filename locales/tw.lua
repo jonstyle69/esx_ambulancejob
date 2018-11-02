ae44c8d5d3c26ea9d42153ab6a651f67233ef1b3
jonstyle69 <44425507+jonstyle69@users.noreply.github.com>
2018-11-02 16:10:37 +0800

    Traditional Chinese translation

---
+++ locales/tw.lua
@@ -0,0 +1,62 @@
+Locales['tw'] = {
+  -- Cloakroom
+  ['cloakroom'] = '更衣室',
+  ['ems_clothes_civil'] = '平民服裝',
+  ['ems_clothes_ems'] = 'EMS救護員服裝',
+  -- Vehicles
+  ['veh_menu'] = '車輛選單',
+  ['veh_spawn'] = '按 ~INPUT_CONTEXT~ 來獲得車輛或直升機',
+  ['store_veh'] = '按 ~INPUT_CONTEXT~ 來儲存車輛',
+  ['ambulance'] = '救護車',
+  ['helicopter'] = '直升機',
+  -- Action Menu
+  ['hospital'] = '醫院',
+  ['revive_inprogress'] = '正在復活!',
+  ['revive_complete'] = '你復活了 ~y~%s~s~',
+  ['revive_complete_award'] = '你復活了 ~y~%s~s~ 並賺到 ~g~$%s~s~!',
+  ['heal_inprogress'] = '你正在治療！',
+  ['heal_complete'] = '你已經痊癒了 ~y~%s~s~',
+  ['no_players'] = '附近沒有玩家',
+  ['no_vehicles'] = '附近沒有車輛',
+  ['player_not_unconscious'] = '該玩家失去意識！',
+  ['player_not_conscious'] = '那玩家沒有生命跡象！',
+  -- Boss Menu
+  ['boss_actions'] = '老闆行動',
+  -- Misc
+  ['invalid_amount'] = '~r~無效金額',
+  ['open_menu'] = '按 ~INPUT_CONTEXT~ 打開選單',
+  ['deposit_amount'] = '存款金額'',
+  ['money_withdraw'] = '取款金額',
+  ['fast_travel'] = '按 on ~INPUT_CONTEXT~ 快速旅行.',
+  ['open_pharmacy'] = '按 on ~INPUT_CONTEXT~ 打開藥局.',
+  ['pharmacy_menu_title'] = '藥局',
+  ['pharmacy_take'] = '拿',
+  ['medikit'] = '醫療包',
+  ['bandage'] = '繃帶',
+  ['max_item'] = '已經足夠了。',
+  -- F6 Menu
+  ['ems_menu'] = 'EMS選單',
+  ['ems_menu_title'] = '救護車 - EMS選單',
+  ['ems_menu_revive'] = '復活玩家',
+  ['ems_menu_putincar'] = '放入車輛',
+  ['ems_menu_small'] = '治愈小傷口',
+  ['ems_menu_big'] = '治療嚴重傷害',
+  -- Phone
+  ['alert_ambulance'] = '救護車警報',
+  -- Death
+  ['please_wait'] = '你會失血過多 ~b~%s 分鐘 %s 秒 ~s~\n',
+  ['press_respawn'] = '按 [~b~E~w~] 重生',
+  ['respawn_now_fine'] = '來重生 ~g~$%s ~w~[按 ~b~E~w~]',
+  ['respawn_fine'] = 'you paid ~r~$%s~s~ 來重生',
+  ['distress_send'] = '按 [~b~G~s~] 發送求救信號',
+  ['distress_sent'] = '已發送到救護單位！請耐心等候!',
+  ['distress_message'] = '需要醫療人員：玩家失去意識！',
+  -- Revive
+  ['revive_help'] = '復活一名玩家',
+  -- Item
+  ['used_medikit'] = '你已用 ~y~1x~s~ 醫療包',
+  ['used_bandage'] = '你已用 ~y~1x~s~ 繃帶',
+  ['not_enough_medikit'] = '你沒有 ~b~醫療包~w~.',
+  ['not_enough_bandage'] = '你沒有 ~b~繃帶~w~.',
+  ['healed'] = '你獲得了治療',
+}
